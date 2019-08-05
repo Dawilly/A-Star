10e948b3edff160ef3a07af35c19f1122a309506
David Weil <gamemx25@gmail.com>
2019-08-05 14:29:34 -0700

    Saving Work in Progress

--- A-Star/AStar.cs
+++ A-Star/AStar.cs
@@ -2,7 +2,29 @@
 
 namespace A_Star
 {
-    public class AStar {
+    public class AStar<T> {
+        public AStar() {
+
+        }
+
+        public void Search(T start, T end) {
+            PriorityQueue<T> pQueue = new PriorityQueue<T>();
+            pQueue.Enqueue(0, start);
+
+            // camefrom and cost so far
+
+            while (pQueue.Size > 0) {
+                T current = pQueue.Dequeue();
+                //if current.Equals(end)
+                    //reconstruct path
+                
+                //T next in neighbors
+                foreach(T next in ) {
+
+                }
+            }
+        }
+
 
     }
 }
